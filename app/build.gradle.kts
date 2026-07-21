f6ceea55c2df69e182ee0889cf8c93cf91e64669
mmuhofy <muhofy.developer@gmail.com>
2026-07-21 04:39:22 +0000

    chore(release): bump to 2.2.0 (versionCode 92), add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 26
         compileSdk = 37
         targetSdk = 36
-        versionCode = 91
-        versionName = "2.1.2"
+        versionCode = 92
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
